1593278d6bcec945145fe1b7505d999b2de1ce15
yaroslav <Viktor1_67@mail.ru>
2017-07-11 11:47:05 +0300

    fix small bugs

--- CompanyExplorerProject/urls.py
+++ CompanyExplorerProject/urls.py
@@ -1,4 +1,4 @@
-"""EastwoodProject URL Configuration
+"""CompanyExplorerProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('CompanyExplorer.urls'))
+    url(r'admin/', admin.site.urls),
+    url(r'^', include('CompanyExplorer.urls'))
 ]
